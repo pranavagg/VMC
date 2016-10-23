52ecf24267e1781fed20b96714174f52674cad6e
Pranav <pranav.aggarwal06@gmail.com>
2016-10-23 20:40:40 +0530

    column name removed from row form

--- src/spreadsheet/forms.py
+++ src/spreadsheet/forms.py
@@ -37,6 +37,20 @@ class ColumnForm(forms.ModelForm):
 
 
 class RowForm(forms.Form):
+
+    @staticmethod
+    def colToExcel(col):  # col is 1 based
+        excelCol = str()
+        div = col
+        while div:
+            (div, mod) = divmod(div-1, 26)  # will return (x, 0 .. 25)
+            excelCol = chr(mod + 65) + excelCol
+        return excelCol
+
+    @staticmethod
+    def get_field(label, disabled):
+        return forms.CharField(max_length=1024, label=label, required=False, disabled=disabled)
+
     def __init__(self, *args, **kwargs):
         columns = kwargs.pop('columns', [])
         add = kwargs.pop('add', True)
@@ -46,20 +60,16 @@ class RowForm(forms.Form):
         super(RowForm, self).__init__(*args, **kwargs)
 
         if add:
-            for column in columns:
-                self.fields['column_' + str(column.id)] = forms.CharField(max_length=1024, label=column.name,
-                                                                          required=False)
+            for col_num, column in enumerate(columns, 1):
+                self.fields['column_' + str(column.id)] = self.get_field(self.colToExcel(col_num), False)
         else:
             last_hour_date_time = timezone.now() - timedelta(hours=1)
-            
-            for column, row in initial_data:
+            for col_num, data in enumerate(initial_data, 1):
+                column, row = data
                 if user.is_authenticated() and user.is_superuser and not user_check_override:
-                    self.fields['column_' + str(column.id)] = forms.CharField(max_length=1024, label=column.name,
-                                                                              required=False)
+                    self.fields['column_' + str(column.id)] = self.get_field(self.colToExcel(col_num), False)
                 else:
                     if row.update_time >= last_hour_date_time:
-                        self.fields['column_' + str(column.id)] = forms.CharField(max_length=1024, label=column.name,
-                                                                                  required=False)
+                        self.fields['column_' + str(column.id)] = self.get_field(self.colToExcel(col_num), False)
                     else:
-                        self.fields['column_' + str(column.id)] = forms.CharField(max_length=1024, label=column.name,
-                                                                                  required=False, disabled=True)
+                        self.fields['column_' + str(column.id)] = self.get_field(self.colToExcel(col_num), True)
